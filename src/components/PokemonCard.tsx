d617d9f840a46f7ea6ef879f44e57ed612e90a8b
pangsui <upangsuilingeh@gmail.com>
2024-10-22 18:33:05 +0200

    branch composant 2 ajoute

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,13 +1,23 @@
 export default function PokemonCard() {
+  const pokemon = pokemonList[0].name;
+  const img = pokemonList[0].imgSrc;
   return (
     <div>
       <figure>
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-          alt="Bulbasau"
-        />
-        <figcaption>Bulbasaur</figcaption>
+        <img src={img} alt={pokemon} />
+        <figcaption>{pokemon}</figcaption>
       </figure>
     </div>
   );
 }
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
